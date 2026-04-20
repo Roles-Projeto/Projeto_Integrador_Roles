749c58229ecb9d818716e887f9a9cbc2ffa48af1
ThaliaAndrade <thalia_andrade_santana@hotmail.com>
2026-04-20 18:31:31 -0300

    subindo docker

--- backend/db/db_config.js
+++ backend/db/db_config.js
@@ -1,19 +1,19 @@
 require("dotenv").config();
 
+const { Pool } = require("pg");
+
+const isProd = !!process.env.DATABASE_URL;
+
 let pool;
 
-if (process.env.NODE_ENV === "production") {
-  // POSTGRES (Render)
-  const { Pool } = require("pg");
-
+if (isProd) {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false }
   });
 
-  console.log("🟢 Usando PostgreSQL (produção)");
+  console.log("🟢 PostgreSQL (Render)");
 } else {
-  // MYSQL (local)
   const mysql = require("mysql2");
 
   pool = mysql.createPool({
@@ -21,12 +21,10 @@ if (process.env.NODE_ENV === "production") {
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10
+    database: process.env.DB_NAME
   });
 
-  console.log("🟡 Usando MySQL (local)");
+  console.log("🟡 MySQL (local)");
 }
 
 module.exports = pool;
